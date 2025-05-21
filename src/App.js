77d7dda5d4c0cb4c117811c1dc3728f33978e943
solsv7 <solsv7@gmail.com>
2025-05-21 17:32:44 -0300

    Guardando cambios antes del merge

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import LoginPage from "./pages/LoginPage";
 import AdminPage from "./pages/AdminPage";
 import ProfesorPage from "./pages/ProfesorPage";
 import AlumnoPage from "./pages/AlumnoPage";
-import StateInfo from "./components/StateInfo";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
